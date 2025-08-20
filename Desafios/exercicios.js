5cd7dbf1278a6224aed9138eaf5755dde677703e
Mariana Souza <marsouzafranca@gmil.com>
2025-08-19 23:11:49 -0300

    feat: adiciona melhorias no jogo com validação, contagem de tentativas e novo jogo

--- Desafios/exercicios.js
+++ Desafios/exercicios.js
@@ -38,3 +38,95 @@ function maiorNumero(a){
     return a * a;
 }
 console.log(maiorNumero(3));
+
+
+
+// 7. Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+function imc(altura, peso){
+    let calculandoImc = peso / (altura * altura);
+    return calculandoImc
+}
+console.log(imc(1.70, 57));
+
+
+// 8. Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+function valorFatorial(numero){
+    let acumulador = 1; 
+    while(numero > 1){ //Ultilizamos o while pois ele só ira rodar se a condição passada nele for verdadeira.
+        acumulador = acumulador * numero;//Nesta linha pegamos o acumulador que é o nosso contador e multiplicamos pelo número (acumulador *= numero; multiplica e atualiza o acumulador) => Jeito simplificado.
+        numero = numero - 1; //A mesma coisa nesta linha, pegamos o número e subtraimos por 1 para que a cada vez que esse código for executado ele diminua um ate chegar em 1  (numero--; diminui o numero) => Jeito simplificado.
+    } //nesses dois casos a cima é necessario salvar as informações em variaveis, pois se não nada irá acontecer 
+    return acumulador;
+}
+console.log(valorFatorial(5));
+
+
+// 9. Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+function valorEmDolar(valor){
+    return valor * 4.80; // Número decimal em JavaScript usa ponto (.)
+}
+console.log(valorEmDolar(10));
+
+
+// 10. Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+function areaEPerimetroRetangulo(altura, largura){
+    let area = altura * largura;
+    let perimetro = 2 * (altura + largura);
+    return {area, perimetro}; //Em um return caso formos retornar duas variaveis não podemos imentar uma na outra dessa maneira => return area, perimetro
+    //Temos algumas formas certas de fazer isso ultilizando um Array => return [area, perimetro]; que não é tão ultilizado ou ultilizando um objeto que é mais comum, ficando assim => return { area: area, perimetro: perimetro}; ou de uma forma mais simplificada => return { area, perimetro };
+}
+let resultadoRetangulo = areaEPerimetroRetangulo(4,5);
+console.log('Área:', resultado.area);
+console.log('Perímetro:', resultado.perimetro);
+
+
+// 11. Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+// Perımetro=2×π×r
+// Area=π×r2
+function areaEPerimetroCirculo(raio){
+    let pi = 3.14;
+    let area = pi * raio **2;
+    let perimetro = 2 * pi * raio;
+    return {area, perimetro};
+}
+let resultadoCirculo = areaEPerimetroCirculo(5);
+console.log('Área', resultadoCirculo.area);
+console.log('Perímetro', resultadoCirculo.perimetro);
+
+
+// 12. Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+function tabuada(numero){
+    // Um loop for em JavaScript tem três partes principais, separadas por ponto e vírgula. São elas => Inicialização, condição e atualização. 
+    for (let contador = 1; contador <= 10; contador++){  
+        let resultado = numero * contador;
+        console.log(`${numero} X ${conatdor} = ${resultado}`)
+    }
+    
+}
+tabuada(5)
+
+
+// 13. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+
+// 14. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+
+// 15. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+
+// 16. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomesEscolhidos = ['Ana', 'Pedro', 'Lucas'];
+console.log(nomesEscolhidos[0]);
+
+// 17. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(nomesEscolhidos[1]);
+
+// 18. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(nomesEscolhidos[2]);
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto =  gerarNumeroAleatorio();  
 let tentativas = 1;
 
@@ -5,15 +7,17 @@ function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 function exibirMensagemInicial() {
     exibirTexto('h1', 'Seja bem vindo(a) ao jogo de número secreto.');
     exibirTexto('p','Escolha um número de 0 a 10');
 }
-exibirMensagemInicial();
 
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value; //O value serve para quando nós queremos verificar apenas o valaor que esta sendo inserido pelo usuario. 
+
     if (chute == numeroSecreto) {
         exibirTexto('h1', 'Você acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativa' : 'tentativas'; //condição ? valorSeVerdadeiro : valorSeFalso
@@ -29,13 +33,23 @@ function verificarChute() {
     }  else {
             exibirTexto('p', 'O número secreto é maior');
     }
-
     tentativas++;
     limparCampo()
     }
 }
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1); //Nesta linha nos geramos o número aleatorio e multiplicamos por 10 e somamos + 1 para que fique de 0 a 10 e não de 0 a 9 
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);//Nesta linha nos geramos o número aleatorio e multiplicamos por 10 e somamos + 1 para que fique de 0 a 10 e não de 0 a 9 
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){ //O includes serve para nós verificarmos se aquele elemento já está na lista.
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido); //Com o push nós adicionamos o elemento desejado dentro da lista.
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
     //O parseInt serve para transformar o número em inteiro
 }
 
@@ -46,8 +60,8 @@ function limparCampo() {
 
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
-    chute = 1;
-    exibirMensagemInicial();
     limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
